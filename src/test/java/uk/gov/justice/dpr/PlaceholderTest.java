e0b6089cefd2f2aba6953394d6651f2f6f3c1188
Carwyn Ellis <125277169+carwynmoj@users.noreply.github.com>
2023-03-08 14:15:12 +0000

    DPR-319 initial repo structure with gradle config

---
+++ src/test/java/uk/gov/justice/dpr/PlaceholderTest.java
@@ -0,0 +1,14 @@
+package uk.gov.justice.dpr;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class PlaceholderTest {
+
+    @Test
+    public void doSomethingReturnsTrue() {
+        assertTrue(Placeholder.doSomething());
+    }
+
+}
