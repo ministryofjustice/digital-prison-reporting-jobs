6a09bef201eb6eaab2b71bd6176e24dec2546a02
Carwyn Ellis <125277169+carwynmoj@users.noreply.github.com>
2023-03-13 14:40:02 +0000

    DPR-319 add preliminary dms event models

--- src/main/java/uk/gov/justice/digital/job/KinesisTestJob.java
+++ src/main/java/uk/gov/justice/digital/job/KinesisTestJob.java
@@ -1,16 +1,24 @@
 package uk.gov.justice.digital.job;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.ObjectReader;
+import com.fasterxml.jackson.databind.PropertyNamingStrategies;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import org.apache.spark.api.java.JavaRDD;
 import org.apache.spark.api.java.function.VoidFunction;
 import uk.gov.justice.digital.client.kinesis.KinesisReader;
 import uk.gov.justice.digital.config.JobParameters;
-
-import java.nio.charset.StandardCharsets;
+import uk.gov.justice.digital.job.model.dms.EventRecord;
 
 /**
  * Test job to explore building a kinesis stream and logging out some metrics.
  */
 public class KinesisTestJob {
+
+    private static final ObjectReader dmsEventReader = new ObjectMapper()
+        .setPropertyNamingStrategy(PropertyNamingStrategies.KEBAB_CASE)
+        .registerModule(new JavaTimeModule())
+        .readerFor(EventRecord.class);
 
     public static void main(String[] args) throws Exception {
         KinesisReader kinesisReader = new KinesisReader(JobParameters.fromGlueJob(), batchProcessor);
@@ -21,8 +29,8 @@ public class KinesisTestJob {
         if (!batch.isEmpty()) {
             System.out.println("Processing batch: " + batch.id() + " with " + batch.count() + " records");
             batch.foreach((VoidFunction<byte[]>) data -> {
-                // TODO - parse data and determine lag.
-                System.out.println("Got record: " + new String(data, StandardCharsets.UTF_8));
+                EventRecord parsedData = dmsEventReader.readValue(data);
+                System.out.println("Parsed event has metadata: " + parsedData);
             });
         }
     };
--- src/main/java/uk/gov/justice/digital/job/model/dms/EventMetadata.java
+++ src/main/java/uk/gov/justice/digital/job/model/dms/EventMetadata.java
@@ -1,2 +1,19 @@
-package uk.gov.justice.digital.job.model.dms;public class EventMetadata {
+package uk.gov.justice.digital.job.model.dms;
+
+import lombok.*;
+import lombok.extern.jackson.Jacksonized;
+
+import java.time.Instant;
+
+@Data
+@Builder
+@Jacksonized
+public class EventMetadata {
+    private Instant timestamp;
+    private String recordType;
+    private String operation;
+    private String partitionKeyType;
+    private String partitionKeyValue;
+    private String schemaName;
+    private String tableName;
 }
