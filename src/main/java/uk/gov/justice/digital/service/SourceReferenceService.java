99a36d930c380e6879da52c4f261e97507a440d4
Carwyn Ellis <125277169+carwynmoj@users.noreply.github.com>
2023-03-16 19:28:40 +0000

    DPR-320 add spark to test deps and simplify SourceReferenceService

--- src/main/java/uk/gov/justice/digital/service/SourceReferenceService.java
+++ src/main/java/uk/gov/justice/digital/service/SourceReferenceService.java
@@ -1,21 +1,14 @@
 package uk.gov.justice.digital.service;
 
+import lombok.val;
+import org.apache.spark.sql.types.DataType;
+import uk.gov.justice.digital.service.model.SourceReference;
+
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.PrintWriter;
 import java.io.StringWriter;
-import java.nio.file.Path;
-import java.nio.file.Paths;
-import java.util.HashMap;
-import java.util.HashSet;
-import java.util.Map;
-import java.util.Set;
-
-import org.apache.hadoop.shaded.com.nimbusds.jose.util.IOUtils;
-import org.apache.spark.sql.types.DataType;
-
-import com.google.common.base.Charsets;
-import uk.gov.justice.digital.service.model.SourceReference;
+import java.util.*;
 
 public class SourceReferenceService {
 
@@ -55,34 +48,22 @@ public class SourceReferenceService {
         return (ref == null ? null : ref.getSchema());
     }
 
+    // TODO - review usages of this
     protected static DataType getSchemaFromResource(final String resource) throws IOException {
-        final InputStream stream = getStream(resource);
-        if(stream != null) {
-            return DataType.fromJson(IOUtils.readInputStreamToString(stream, Charsets.UTF_8));
-        }
-        return null;
+        return Optional.ofNullable(System.class.getResourceAsStream(resource))
+            .map(SourceReferenceService::readInputStream)
+            .map(DataType::fromJson)
+            .orElseThrow(() -> new IllegalStateException("Failed to read resource: " + resource));
+    }
+
+    private static String readInputStream(InputStream is) {
+        return new Scanner(is, "UTF-8")
+            .useDelimiter("\\A") // Matches the next boundary which in our case will be EOF.
+            .next();
     }
 
     public static Set<SourceReference> getReferences() {
         return new HashSet<SourceReference>(sources.values());
-    }
-
-    protected static InputStream getStream(final String resource) {
-        InputStream stream = System.class.getResourceAsStream(resource);
-        if(stream == null) {
-            stream = System.class.getResourceAsStream("/src/main/resources" + resource);
-            if(stream == null) {
-                stream = System.class.getResourceAsStream("/target/classes" + resource);
-                if(stream == null) {
-                    Path root = Paths.get(".").normalize().toAbsolutePath();
-                    stream = System.class.getResourceAsStream(root.toString() + "/src/main/resources" + resource);
-                    if(stream == null) {
-                        stream = SourceReferenceService.class.getResourceAsStream(resource);
-                    }
-                }
-            }
-        }
-        return stream;
     }
 
     protected static void handleError(final Exception e) {
