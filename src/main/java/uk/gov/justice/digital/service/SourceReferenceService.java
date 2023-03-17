3391d7df512dc52bb38153db8ff4b650f9fa2761
Carwyn Ellis <125277169+carwynmoj@users.noreply.github.com>
2023-03-17 08:57:57 +0000

    DPR-320 minor tidyup of the SourceReferenceService

--- src/main/java/uk/gov/justice/digital/service/SourceReferenceService.java
+++ src/main/java/uk/gov/justice/digital/service/SourceReferenceService.java
@@ -1,32 +1,32 @@
 package uk.gov.justice.digital.service;
 
-import lombok.val;
+import io.micronaut.context.annotation.Bean;
 import org.apache.spark.sql.types.DataType;
 import uk.gov.justice.digital.service.model.SourceReference;
 
-import java.io.IOException;
 import java.io.InputStream;
-import java.io.PrintWriter;
-import java.io.StringWriter;
 import java.util.*;
 
+/**
+ * Temporary Internal Schema storage using json schema files.
+ *
+ * This will be replaced by the Hive Metastore in later work.
+ *
+ * See DPR-246 for further details.
+ */
+@Bean
 public class SourceReferenceService {
 
     private static final Map<String, SourceReference> sources = new HashMap<String,SourceReference>();
 
-    // Temporarily hardcoded until we extract this into s3/Dynamo/???
     static {
-        try {
-            sources.put("oms_owner.offenders", new SourceReference("SYSTEM.OFFENDERS", "nomis", "offenders", "OFFENDER_ID", getSchemaFromResource("/schemas/oms_owner.offenders.schema.json")));
-            sources.put("oms_owner.offender_bookings", new SourceReference("SYSTEM.OFFENDER_BOOKINGS", "nomis", "offender_bookings", "OFFENDER_BOOK_ID", getSchemaFromResource("/schemas/oms_owner.offender_bookings.schema.json")));
-            sources.put("oms_owner.agency_locations", new SourceReference("SYSTEM.AGENCY_LOCATIONS", "nomis", "agency_locations", "AGY_LOC_ID", getSchemaFromResource("/schemas/oms_owner.agency_locations.schema.json")));
-            sources.put("oms_owner.agency_internal_locations", new SourceReference("SYSTEM.AGENCY_INTERNAL_LOCATIONS", "nomis", "agency_internal_locations", "INTERNAL_LOCATION_ID", getSchemaFromResource("/schemas/oms_owner.agency_internal_locations.schema.json")));
+        sources.put("oms_owner.offenders", new SourceReference("SYSTEM.OFFENDERS", "nomis", "offenders", "OFFENDER_ID", getSchemaFromResource("/schemas/oms_owner.offenders.schema.json")));
+        sources.put("oms_owner.offender_bookings", new SourceReference("SYSTEM.OFFENDER_BOOKINGS", "nomis", "offender_bookings", "OFFENDER_BOOK_ID", getSchemaFromResource("/schemas/oms_owner.offender_bookings.schema.json")));
+        sources.put("oms_owner.agency_locations", new SourceReference("SYSTEM.AGENCY_LOCATIONS", "nomis", "agency_locations", "AGY_LOC_ID", getSchemaFromResource("/schemas/oms_owner.agency_locations.schema.json")));
+        sources.put("oms_owner.agency_internal_locations", new SourceReference("SYSTEM.AGENCY_INTERNAL_LOCATIONS", "nomis", "agency_internal_locations", "INTERNAL_LOCATION_ID", getSchemaFromResource("/schemas/oms_owner.agency_internal_locations.schema.json")));
 
-            sources.put("public.offenders", new SourceReference("SYSTEM.OFFENDERS", "public", "offenders", "OFFENDER_ID", getSchemaFromResource("/schemas/oms_owner.offenders.schema.json")));
-            sources.put("public.offender_bookings", new SourceReference("SYSTEM.OFFENDER_BOOKINGS", "public", "offender_bookings", "OFFENDER_BOOK_ID", getSchemaFromResource("/schemas/oms_owner.offender_bookings.schema.json")));
-        } catch (IOException e) {
-            handleError(e);
-        }
+        sources.put("public.offenders", new SourceReference("SYSTEM.OFFENDERS", "public", "offenders", "OFFENDER_ID", getSchemaFromResource("/schemas/oms_owner.offenders.schema.json")));
+        sources.put("public.offender_bookings", new SourceReference("SYSTEM.OFFENDER_BOOKINGS", "public", "offender_bookings", "OFFENDER_BOOK_ID", getSchemaFromResource("/schemas/oms_owner.offender_bookings.schema.json")));
     }
 
     public static String getPrimaryKey(final String key) {
@@ -43,13 +43,12 @@ public class SourceReferenceService {
         final SourceReference ref = sources.get(key.toLowerCase());
         return (ref == null ? null : ref.getTable());
     }
-    public static DataType getSchema(final String key) {
+    public DataType getSchema(final String key) {
         final SourceReference ref = sources.get(key.toLowerCase());
         return (ref == null ? null : ref.getSchema());
     }
 
-    // TODO - review usages of this
-    protected static DataType getSchemaFromResource(final String resource) throws IOException {
+    private static DataType getSchemaFromResource(final String resource) {
         return Optional.ofNullable(System.class.getResourceAsStream(resource))
             .map(SourceReferenceService::readInputStream)
             .map(DataType::fromJson)
@@ -62,15 +61,8 @@ public class SourceReferenceService {
             .next();
     }
 
-    public static Set<SourceReference> getReferences() {
+    public Set<SourceReference> getReferences() {
         return new HashSet<SourceReference>(sources.values());
-    }
-
-    protected static void handleError(final Exception e) {
-        final StringWriter sw = new StringWriter();
-        final PrintWriter pw = new PrintWriter(sw);
-        e.printStackTrace(pw);
-        System.err.print(sw.getBuffer().toString());
     }
 
 }
