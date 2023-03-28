0bd7f1014040342b213f5374fac5b7bde0861610
Carwyn Ellis <125277169+carwynmoj@users.noreply.github.com>
2023-03-28 15:11:22 +0100

    DPR-320 apply strict parsing to incoming dms records

--- src/main/java/uk/gov/justice/digital/converter/Converter.java
+++ src/main/java/uk/gov/justice/digital/converter/Converter.java
@@ -4,7 +4,20 @@ import org.apache.spark.api.java.JavaRDD;
 import org.apache.spark.sql.Dataset;
 import org.apache.spark.sql.Row;
 import org.apache.spark.sql.SparkSession;
+import org.apache.spark.sql.types.StructType;
 
-public interface Converter {
-    Dataset<Row> convert(JavaRDD<Row> input, SparkSession spark);
+import static org.apache.spark.sql.types.DataTypes.StringType;
+import static uk.gov.justice.digital.job.model.Columns.*;
+
+public abstract class Converter {
+
+    // This schema defines the common output format to be created from the incoming data.
+    protected static final StructType PARSED_DATA_SCHEMA = new StructType()
+        .add(DATA, StringType, false)
+        .add(SOURCE, StringType, false)
+        .add(TABLE, StringType, false)
+        .add(OPERATION, StringType, false);
+
+    public abstract Dataset<Row> convert(JavaRDD<Row> input, SparkSession spark);
+
 }
