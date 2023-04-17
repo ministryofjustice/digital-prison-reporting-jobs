827056a95d16e5f089353c0d1a5047d14265f2e4
Hari Chintala <hari.chintala@digital.justice.gov.uk>
2023-04-17 07:23:06 +0100

    DPR-293, Verify Gradle Run

---
+++ src/main/java/uk/gov/justice/digital/Placeholder.java
@@ -0,0 +1,13 @@
+package uk.gov.justice.digital;
+
+// Placeholder class with test to facilitate CI/CD setup.
+public class Placeholder {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+
+    public static boolean doSomething() {
+        return true;
+    }
+
+}
