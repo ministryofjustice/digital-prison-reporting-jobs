de6164294db7cac55f979f6c495c2c85c2e3971a
Carwyn Ellis <125277169+carwynmoj@users.noreply.github.com>
2023-05-16 09:59:15 +0100

    DPR-422 remove unused JobClient and tests

--- src/main/java/uk/gov/justice/digital/config/JobArguments.java
+++ src/main/java/uk/gov/justice/digital/config/JobArguments.java
@@ -17,26 +17,31 @@ import java.util.Map;
 import java.util.Optional;
 import java.util.stream.Collectors;
 
+/**
+ * Class that defines and provides access to the job arguments that we support.
+ *
+ * Arguments are taken from the values parsed by the CommandLinePropertySource.
+ */
 @Singleton
 public class JobArguments {
 
     private static final Logger logger = LoggerFactory.getLogger(JobArguments.class);
 
-    public static final String AWS_REGION = "dpr.aws.region";
-    public static final String AWS_KINESIS_ENDPOINT_URL = "dpr.aws.kinesis.endpointUrl";
     public static final String AWS_DYNAMODB_ENDPOINT_URL = "dpr.aws.dynamodb.endpointUrl";
-    public static final String KINESIS_READER_STREAM_NAME = "dpr.kinesis.reader.streamName";
-    public static final String KINESIS_READER_BATCH_DURATION_SECONDS = "dpr.kinesis.reader.batchDurationSeconds";
-    public static final String RAW_S3_PATH = "dpr.raw.s3.path";
-    public static final String STRUCTURED_S3_PATH = "dpr.structured.s3.path";
-    public static final String VIOLATIONS_S3_PATH = "dpr.violations.s3.path";
+    public static final String AWS_KINESIS_ENDPOINT_URL = "dpr.aws.kinesis.endpointUrl";
+    public static final String AWS_REGION = "dpr.aws.region";
     public static final String CURATED_S3_PATH = "dpr.curated.s3.path";
+    public static final String DOMAIN_CATALOG_DATABASE_NAME = "dpr.domain.catalog.db";
     public static final String DOMAIN_NAME = "dpr.domain.name";
-    public static final String DOMAIN_TABLE_NAME = "dpr.domain.table.name";
     public static final String DOMAIN_OPERATION = "dpr.domain.operation";
     public static final String DOMAIN_REGISTRY = "dpr.domain.registry";
     public static final String DOMAIN_S3_TARGET_PATH = "dpr.domain.target.path";
-    public static final String DOMAIN_CATALOG_DATABASE_NAME = "dpr.domain.catalog.db";
+    public static final String DOMAIN_TABLE_NAME = "dpr.domain.table.name";
+    public static final String KINESIS_READER_BATCH_DURATION_SECONDS = "dpr.kinesis.reader.batchDurationSeconds";
+    public static final String KINESIS_READER_STREAM_NAME = "dpr.kinesis.reader.streamName";
+    public static final String RAW_S3_PATH = "dpr.raw.s3.path";
+    public static final String STRUCTURED_S3_PATH = "dpr.structured.s3.path";
+    public static final String VIOLATIONS_S3_PATH = "dpr.violations.s3.path";
 
     private final Map<String, String> config;
 
@@ -136,6 +141,10 @@ public class JobArguments {
         return new SimpleEntry<>(cleanedKey, entry.getValue());
     }
 
+    // Where command line arguments are present Micronaut will create a CommandLinePropertySource instance which
+    // parses any arguments and makes them available directly on the context via the getProperty() method (the args
+    // are combined with properties) or via the CommandLinePropertySource instance on the environment. We prefer the
+    // latter since this property source will *only* contain the arguments passed to the command.
     private static Map<String, String> getCommandLineArgumentsFromContext(ApplicationContext context) {
         return context.getEnvironment()
                 .getPropertySources()
@@ -152,6 +161,7 @@ public class JobArguments {
         return p.getName().equals(CommandLinePropertySource.NAME);
     }
 
+    // We need to cast up to CommandLinePropertySource in order to get access to the `asMap` method.
     private static Optional<CommandLinePropertySource> castToCommandLinePropertySource(PropertySource p) {
         return (p instanceof CommandLinePropertySource)
                 ? Optional.of((CommandLinePropertySource) p)
