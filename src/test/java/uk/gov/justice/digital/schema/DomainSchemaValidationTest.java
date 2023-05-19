4ab66829b7e09655ae4d74fbd163bd6fb5e38627
Carwyn Ellis <125277169+carwynmoj@users.noreply.github.com>
2023-05-19 10:58:22 +0100

    DPR-476 rename domain schema to include contract and add initial test

---
+++ src/test/java/uk/gov/justice/digital/schema/DomainSchemaValidationTest.java
@@ -0,0 +1,17 @@
+package uk.gov.justice.digital.schema;
+
+import org.junit.jupiter.api.Test;
+
+// Test to support construction of a JSON schema to validate domain schema files.
+public class DomainSchemaValidationTest {
+
+    private static final String RESOURCE_PATH = "schemas";
+    private static final String AGENCY_INTERNAL_LOCATIONS_SCHEMA = "agency-internal-locations-schema.json";
+    private static final String DOMAIN_CONTRACT_SCHEMA = "domain-contract-schema.json";
+
+    @Test
+    public void agencyInternalLocationsSchemaShouldValidateAgainstDomainContractSchema() {
+
+    }
+    
+}
