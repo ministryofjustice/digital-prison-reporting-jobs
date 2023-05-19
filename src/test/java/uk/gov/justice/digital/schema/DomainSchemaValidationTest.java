2898541d04739c703b43ac4d3270273fdd81b28d
Carwyn Ellis <125277169+carwynmoj@users.noreply.github.com>
2023-05-19 11:58:55 +0100

    DPR-476 Add validator dependency and initial test case

--- src/test/java/uk/gov/justice/digital/schema/DomainSchemaValidationTest.java
+++ src/test/java/uk/gov/justice/digital/schema/DomainSchemaValidationTest.java
@@ -1,17 +1,34 @@
 package uk.gov.justice.digital.schema;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.networknt.schema.JsonSchemaFactory;
+import com.networknt.schema.SpecVersion;
+import lombok.val;
 import org.junit.jupiter.api.Test;
+import uk.gov.justice.digital.test.ResourceLoader;
 
-// Test to support construction of a JSON schema to validate domain schema files.
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 public class DomainSchemaValidationTest {
 
-    private static final String RESOURCE_PATH = "schemas";
+    private static final String RESOURCE_PATH = "/schemas";
     private static final String AGENCY_INTERNAL_LOCATIONS_SCHEMA = "agency-internal-locations-schema.json";
     private static final String DOMAIN_CONTRACT_SCHEMA = "domain-contract-schema.json";
 
-    @Test
-    public void agencyInternalLocationsSchemaShouldValidateAgainstDomainContractSchema() {
+    private static final ObjectMapper mapper = new ObjectMapper();
 
+    @Test
+    public void agencyInternalLocationsSchemaShouldPassValidation() throws JsonProcessingException {
+        val validator = JsonSchemaFactory
+                .getInstance(SpecVersion.VersionFlag.V202012)
+                .getSchema(ResourceLoader.getResource(RESOURCE_PATH + "/" + DOMAIN_CONTRACT_SCHEMA));
+
+        val schema = mapper.readTree(ResourceLoader.getResource(RESOURCE_PATH + "/" + AGENCY_INTERNAL_LOCATIONS_SCHEMA));
+
+        val validationErrors = validator.validate(schema);
+
+        assertTrue(validationErrors.isEmpty());
     }
-    
+
 }
