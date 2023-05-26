f9fdc3a76863497e81c6eac955f79181e6883254
Carwyn Ellis <125277169+carwynmoj@users.noreply.github.com>
2023-05-26 17:32:55 +0100

    DPR-478 converter now supports avro simple and logical types with caveats

--- src/main/java/uk/gov/justice/digital/converter/avro/AvroToSparkSchemaConverter.java
+++ src/main/java/uk/gov/justice/digital/converter/avro/AvroToSparkSchemaConverter.java
@@ -3,24 +3,66 @@ package uk.gov.justice.digital.converter.avro;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import jakarta.inject.Singleton;
 import lombok.val;
-import org.apache.spark.sql.types.DataTypes;
-import org.apache.spark.sql.types.Metadata;
-import org.apache.spark.sql.types.StructField;
-import org.apache.spark.sql.types.StructType;
+import org.apache.avro.LogicalType;
+import org.apache.avro.LogicalTypes;
+import org.apache.avro.Schema;
+import org.apache.spark.sql.types.*;
 
+import java.util.AbstractMap.SimpleEntry;
 import java.util.Iterator;
+import java.util.Map;
+import java.util.Optional;
+import java.util.stream.Collectors;
 import java.util.stream.Stream;
 import java.util.stream.StreamSupport;
 
+@Singleton
 public class AvroToSparkSchemaConverter {
+
+    // TODO - revert to new SimpleEntry<> as per the tests - mapEntry isn't really adding much
+    private static final Map<String, DataType> avroSimpleTypeNameToSparkType = Stream.of(
+            mapEntry(Schema.Type.BOOLEAN,   DataTypes.BooleanType),
+            mapEntry(Schema.Type.BYTES,     DataTypes.ByteType),
+            mapEntry(Schema.Type.DOUBLE,    DataTypes.DoubleType),
+            mapEntry(Schema.Type.FLOAT,     DataTypes.FloatType),
+            mapEntry(Schema.Type.INT,       DataTypes.IntegerType),
+            mapEntry(Schema.Type.LONG,      DataTypes.LongType),
+            mapEntry(Schema.Type.NULL,      DataTypes.NullType),
+            mapEntry(Schema.Type.STRING,    DataTypes.StringType)
+    ).collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue));
+
+    private static final Map<String, DataType> avroLogicalTypeNameToSparkType = Stream.of(
+            mapEntry(LogicalTypes.date(),               DataTypes.DateType),
+            mapEntry(LogicalTypes.timestampMicros(),    DataTypes.TimestampType),
+            mapEntry(LogicalTypes.timestampMillis(),    DataTypes.TimestampType),
+            // TODO - there is no time type in spark so for now we use the timestamp type.
+            //      - conversion will need to be handled later during the normalisation stage.
+            mapEntry(LogicalTypes.timeMillis(),         DataTypes.TimestampType),
+            mapEntry(LogicalTypes.timeMicros(),         DataTypes.TimestampType)
+    ).collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue));
 
     private static final ObjectMapper objectMapper = new ObjectMapper();
 
-    public StructType convert(String avroSchema) throws JsonProcessingException {
-        val parsedSchema = objectMapper.readTree(avroSchema);
-        val avroFields = parsedSchema.get("fields").elements();
-        return buildStructTypeFromAvroFields(avroFields);
+    public StructType convert(String avroSchema) {
+        try {
+            val parsedSchema = objectMapper.readTree(avroSchema);
+            val avroFields = parsedSchema.get("fields").elements();
+            return buildStructTypeFromAvroFields(avroFields);
+        }
+        catch (JsonProcessingException jpe) {
+            // TODO - define a custom RTE e.g. SchemaConverterError?
+            throw new RuntimeException("Unexpected error when processing schema", jpe);
+        }
+    }
+
+    private static SimpleEntry<String, DataType> mapEntry(Schema.Type avroType, DataType sparkType) {
+        return new SimpleEntry<>(avroType.getName(), sparkType);
+    }
+
+    private static SimpleEntry<String, DataType> mapEntry(LogicalType avroType, DataType sparkType) {
+        return new SimpleEntry<>(avroType.getName(), sparkType);
     }
 
     private StructType buildStructTypeFromAvroFields(Iterator<JsonNode> fields) {
@@ -39,17 +81,43 @@ public class AvroToSparkSchemaConverter {
 
     private StructField toStructField(JsonNode node) {
         val name = node.get("name").asText();
-        val type = node.get("type").asText();
         val nullable = node.get("nullable").asBoolean();
+        val typeNode = node.get("type");
+        // TODO - review this - what if it's another type
+        val type = (typeNode.isTextual())
+                ? typeNode.asText()
+                : typeNode.get("logicalType").asText();
 
-        if (type.equals("string")) {
+        if (isSimpleType(type)) {
             return new StructField(
                     name,
-                    DataTypes.StringType,
+                    avroSimpleTypeNameToSparkType.get(type),
+                    nullable,
+                    Metadata.empty());
+        }
+        else if (isEnumType(type)) {
+            throw new RuntimeException("ENUM handling ot implemented yet");
+        }
+        else if (isLogicalType(type)) {
+            return new StructField(
+                    name,
+                    avroLogicalTypeNameToSparkType.get(type),
                     nullable,
                     Metadata.empty());
         }
         else throw new RuntimeException("Unsupported avro field type: " + type);
+    }
+
+    private boolean isSimpleType(String type) {
+        return avroSimpleTypeNameToSparkType.containsKey(type);
+    }
+
+    private boolean isEnumType(String type) {
+        return Schema.Type.ENUM.getName().equals(type);
+    }
+
+    private boolean isLogicalType(String logicalType) {
+        return avroLogicalTypeNameToSparkType.containsKey(logicalType);
     }
 
 }
--- src/test/java/uk/gov/justice/digital/converter/avro/AvroToSparkSchemaConverterTest.java
+++ src/test/java/uk/gov/justice/digital/converter/avro/AvroToSparkSchemaConverterTest.java
@@ -1,9 +1,15 @@
 package uk.gov.justice.digital.converter.avro;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
 import lombok.val;
+import org.apache.spark.sql.types.DataType;
+import org.apache.spark.sql.types.DataTypes;
 import org.apache.spark.sql.types.StructType;
 import org.junit.jupiter.api.Test;
+
+import java.util.AbstractMap.SimpleEntry;
+import java.util.Map;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -12,20 +18,69 @@ class AvroToSparkSchemaConverterTest {
     private static final AvroToSparkSchemaConverter underTest = new AvroToSparkSchemaConverter();
 
     @Test
-    public void shouldConvertAValidSchemaWithASimpleTypeDeclaration() throws JsonProcessingException {
+    public void shouldConvertAllSimpleFieldTypesToCorrectSparkType() {
+        val typeMappings = Stream.of(
+                new SimpleEntry<>("boolean", DataTypes.BooleanType),
+                new SimpleEntry<>("bytes",   DataTypes.ByteType),
+                new SimpleEntry<>("double",  DataTypes.DoubleType),
+                new SimpleEntry<>("float",   DataTypes.FloatType),
+                new SimpleEntry<>("int",     DataTypes.IntegerType),
+                new SimpleEntry<>("long",    DataTypes.LongType),
+                new SimpleEntry<>("null",    DataTypes.NullType),
+                new SimpleEntry<>("string",  DataTypes.StringType)
+        ).collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue));
 
+        typeMappings.forEach((avroType, sparkType) -> {
+            val avroSchema = avroSchemaWithSimpleType(avroType);
+            val expectedSchema = sparkSchemaWithType(sparkType);
+            val result = underTest.convert(avroSchema);
+            assertEquals(expectedSchema, result);
+        });
+    }
+
+    @Test
+    public void shouldConvertAllLogicalFieldTypesToCorrectSparkType() {
+        val typeMappings = Stream.of(
+                new SimpleEntry<>("date",             DataTypes.DateType),
+                new SimpleEntry<>("timestamp-millis", DataTypes.TimestampType),
+                new SimpleEntry<>("timestamp-micros", DataTypes.TimestampType),
+                new SimpleEntry<>("time-micros",      DataTypes.TimestampType),
+                new SimpleEntry<>("time-millis",      DataTypes.TimestampType)
+        ).collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue));
+
+        typeMappings.forEach((avroType, sparkType) -> {
+            val avroSchema = avroSchemaWithLogicalType(avroType);
+            val expectedSchema = sparkSchemaWithType(sparkType);
+            val result = underTest.convert(avroSchema);
+            assertEquals(expectedSchema, result);
+        });
+    }
+
+    private String avroSchemaWithSimpleType(String type) {
         val fakeSchema = "{ " +
-                "\"fields\": [ " +
-                    "{ " +
-                    "\"name\": \"some-field\", " +
-                    "\"type\": \"string\", " +
+                "\"fields\": [{ " +
+                    "\"name\": \"aField\", " +
+                    "\"type\": \"TYPE\", " +
                     "\"nullable\": false " +
-                    " }]} ";
+                " }]} ";
+       return fakeSchema.replace("TYPE", type);
+    }
 
-        val result = underTest.convert(fakeSchema);
-        val expectedSchema = new StructType().add("some-field", "string", false);
+    private String avroSchemaWithLogicalType(String type) {
+        val fakeSchema = "{ " +
+                "\"fields\": [{ " +
+                    "\"name\": \"aField\", " +
+                    "\"type\": { " +
+                        "\"type\": \"int\", " +
+                        "\"logicalType\": \"TYPE\" " +
+                    "}, " +
+                    "\"nullable\": false " +
+                " }]} ";
+        return fakeSchema.replace("TYPE", type);
+    }
 
-        assertEquals(expectedSchema, result);
+    private StructType sparkSchemaWithType(DataType type) {
+        return new StructType().add("aField", type, false);
     }
 
 }
