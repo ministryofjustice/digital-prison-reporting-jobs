8721c2c36d25c36d6f6e4f0cf2ee6ced3520b8a0
Karthik Nagaraj <karthik.nagaraj@digital.justice.gov.uk>
2023-06-16 08:50:05 +0100

    DPR-447: unit and integration test streamlined

--- src/it/java/uk/gov/justice/digital/domain/schema/DomainContractValidationIntegrationTest.java
+++ src/it/java/uk/gov/justice/digital/domain/schema/DomainContractValidationIntegrationTest.java
@@ -10,6 +10,7 @@ import org.junit.jupiter.api.Test;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static uk.gov.justice.digital.test.ResourceLoader.getResource;
 
