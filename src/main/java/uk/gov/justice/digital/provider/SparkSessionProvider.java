409c02df4cf457bab9d02cb1419a4c713a7cc3ac
Tom Ogle <tom.ogle@digital.justice.gov.uk>
2023-09-15 17:49:36 +0100

    DPR-677: Enable worker WAL

--- src/main/java/uk/gov/justice/digital/provider/SparkSessionProvider.java
+++ src/main/java/uk/gov/justice/digital/provider/SparkSessionProvider.java
@@ -21,6 +21,7 @@ public class SparkSessionProvider {
                 .set("spark.databricks.delta.optimizeWrite.enabled", "true")
                 .set("spark.databricks.delta.schema.autoMerge.enabled", "true")
                 .set("spark.streaming.stopGracefullyOnShutdown", "true")
+                .set("spark.streaming.receiver.writeAheadLog.enable", "true")
                 .set("spark.sql.catalog.spark_catalog", "org.apache.spark.sql.delta.catalog.DeltaCatalog")
                 .set("spark.sql.extensions", "io.delta.sql.DeltaSparkSessionExtension")
                 .set("spark.sql.legacy.charVarcharAsString", "true")
