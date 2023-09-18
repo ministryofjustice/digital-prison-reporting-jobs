ba580c104fc7807c2e9af5cd6fd9a89536b73c99
Tom Ogle <tom.ogle@digital.justice.gov.uk>
2023-09-18 14:55:04 +0100

    DPR-677 Fix test compilation issue

--- src/test/java/uk/gov/justice/digital/config/BaseSparkTest.java
+++ src/test/java/uk/gov/justice/digital/config/BaseSparkTest.java
@@ -29,7 +29,7 @@ public class BaseSparkTest {
 	protected static SparkSession spark;
 
 	private static SparkSession createSparkSession() {
-		return spark = sparkSessionProvider.getConfiguredSparkSession(sparkTestConfiguration, LogLevel.INFO);
+		return spark = sparkSessionProvider.getConfiguredSparkSession(sparkTestConfiguration, LogLevel.INFO, false);
 	}
 
 	@BeforeAll
