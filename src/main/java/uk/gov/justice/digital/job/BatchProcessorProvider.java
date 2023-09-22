968a2eaf84af3218d136810c1cd9a09ab69072f5
Tom Ogle <tom.ogle@digital.justice.gov.uk>
2023-09-22 10:57:35 +0100

    Revert "DPR2-110: Infer and log input schema."
    This reverts commit 57e3391e3ebd2a1842be01cf02376802fc8c7800.

--- src/main/java/uk/gov/justice/digital/job/BatchProcessorProvider.java
+++ src/main/java/uk/gov/justice/digital/job/BatchProcessorProvider.java
@@ -58,7 +58,6 @@ public class BatchProcessorProvider {
             } else {
                 long cnt = batch.count();
                 logger.info("Batch saw {} records", cnt);
-                logger.info(batch.schema().treeString());
                 batch.limit(2).foreach(row -> {
                     logger.info(row.toString());
                 });
