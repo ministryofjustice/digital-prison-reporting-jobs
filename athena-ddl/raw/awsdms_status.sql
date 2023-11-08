f7d3c604a30ba67cfa59ca29247e05bff4e22326
Tom Ogle <tom.ogle@digital.justice.gov.uk>
2023-11-08 14:29:14 +0000

    DPR2-165: Add DDL for raw awsdms_status deltas

---
+++ athena-ddl/raw/awsdms_status.sql
@@ -0,0 +1,27 @@
+CREATE EXTERNAL TABLE `dms_raw`.`awsdms_status`
+(
+    `Op`                       string COMMENT '',
+    `_timestamp`               string COMMENT '',
+    `server_name`              string COMMENT '',
+    `task_name`                string COMMENT '',
+    `task_status`              string COMMENT '',
+    `status_time`              timestamp COMMENT '',
+    `pending_changes`          bigint COMMENT '',
+    `disk_swap_size`           bigint COMMENT '',
+    `task_memory`              bigint COMMENT '',
+    `source_current_position`  string COMMENT '',
+    `source_current_timestamp` timestamp COMMENT '',
+    `source_tail_position`     string COMMENT '',
+    `source_tail_timestamp`    timestamp COMMENT '',
+    `source_timestamp_applied` timestamp COMMENT ''
+)
+    ROW FORMAT SERDE
+        'org.apache.hadoop.hive.ql.io.parquet.serde.ParquetHiveSerDe'
+    STORED AS INPUTFORMAT
+        'org.apache.hadoop.hive.ql.io.parquet.MapredParquetInputFormat'
+        OUTPUTFORMAT
+            'org.apache.hadoop.hive.ql.io.parquet.MapredParquetOutputFormat'
+    LOCATION
+        's3://dpr-dms-raw-zone-development/awsdms_status/'
+    TBLPROPERTIES (
+        'classification' = 'parquet')
