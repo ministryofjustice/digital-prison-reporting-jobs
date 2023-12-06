8577b529de4f9af1159ae8023be9f11ea8927121
Tom Ogle <tom.ogle@digital.justice.gov.uk>
2023-12-06 17:02:59 +0000

    DPR2-210 comment

--- src/main/java/uk/gov/justice/digital/exception/NoSchemaNoDataException.java
+++ src/main/java/uk/gov/justice/digital/exception/NoSchemaNoDataException.java
@@ -1,5 +1,8 @@
 package uk.gov.justice.digital.exception;
 
+/**
+ * Thrown when there is no data to read and no schema to use to watch for the data to arrive.
+ */
 public class NoSchemaNoDataException extends Exception {
 
     private static final long serialVersionUID = -238644603056367129L;
