eaf2c0c56b8b6bf0c49f13098af2afbd1b50ada5
Tom Ogle <tom.ogle@digital.justice.gov.uk>
2024-06-26 15:32:28 +0100

    DPR2-894: Remove unused import.

--- src/test/java/uk/gov/justice/digital/service/operationaldatastore/OperationalDataStoreServiceTest.java
+++ src/test/java/uk/gov/justice/digital/service/operationaldatastore/OperationalDataStoreServiceTest.java
@@ -10,7 +10,6 @@ import org.mockito.junit.jupiter.MockitoExtension;
 import uk.gov.justice.digital.datahub.model.SourceReference;
 
 import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
