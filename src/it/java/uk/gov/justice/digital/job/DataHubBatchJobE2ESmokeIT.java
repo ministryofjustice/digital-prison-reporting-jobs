eac39c3c960f205bd196e62850eefa69acefa0ca
Tom Ogle <tom.ogle@digital.justice.gov.uk>
2024-07-03 15:14:52 +0100

    DPR2-895: try single test using E2ETestBase

--- src/it/java/uk/gov/justice/digital/job/DataHubBatchJobE2ESmokeIT.java
+++ src/it/java/uk/gov/justice/digital/job/DataHubBatchJobE2ESmokeIT.java
@@ -9,8 +9,6 @@ import org.mockito.junit.jupiter.MockitoExtension;
 import uk.gov.justice.digital.client.s3.S3DataProvider;
 import uk.gov.justice.digital.config.JobArguments;
 import uk.gov.justice.digital.config.JobProperties;
-import uk.gov.justice.digital.datahub.model.OperationalDataStoreConnectionDetails;
-import uk.gov.justice.digital.datahub.model.OperationalDataStoreCredentials;
 import uk.gov.justice.digital.job.batchprocessing.BatchProcessor;
 import uk.gov.justice.digital.provider.SparkSessionProvider;
 import uk.gov.justice.digital.service.ConfigService;
@@ -23,8 +21,8 @@ import uk.gov.justice.digital.service.operationaldatastore.ConnectionPoolProvide
 import uk.gov.justice.digital.service.operationaldatastore.OperationalDataStoreConnectionDetailsService;
 import uk.gov.justice.digital.service.operationaldatastore.OperationalDataStoreDataAccess;
 import uk.gov.justice.digital.service.operationaldatastore.OperationalDataStoreService;
-import uk.gov.justice.digital.service.operationaldatastore.OperationalDataStoreTransformation;
 import uk.gov.justice.digital.service.operationaldatastore.OperationalDataStoreServiceImpl;
+import uk.gov.justice.digital.service.operationaldatastore.OperationalDataStoreTransformation;
 import uk.gov.justice.digital.zone.curated.CuratedZoneLoad;
 import uk.gov.justice.digital.zone.structured.StructuredZoneLoad;
 
@@ -44,103 +42,103 @@ import static uk.gov.justice.digital.test.MinimalTestData.createRow;
  * * Using the file system instead of S3.
  * * Using a test SparkSession.
  */
-@ExtendWith(MockitoExtension.class)
+//@ExtendWith(MockitoExtension.class)
 class DataHubBatchJobE2ESmokeIT extends E2ETestBase {
-    @Mock
-    private JobArguments arguments;
-    @Mock
-    private SourceReferenceService sourceReferenceService;
-    @Mock
-    private ConfigService configService;
-    @Mock
-    private OperationalDataStoreConnectionDetailsService connectionDetailsService;
-    private DataHubBatchJob underTest;
-
-
-    @BeforeEach
-    public void setUp() throws SQLException {
-        givenDatastoreCredentials(connectionDetailsService);
-        givenPathsAreConfigured(arguments);
-        givenTableConfigIsConfigured(arguments, configService);
-        givenGlobPatternIsConfigured();
-        givenRetrySettingsAreConfigured(arguments);
-        givenDependenciesAreInjected();
-        givenSchemaExists(inputSchemaName);
-    }
-
-    @Test
-    public void shouldRunTheJobEndToEndApplyingSomeCDCMessagesAndWritingViolations() throws SQLException {
-        givenASourceReferenceFor(agencyInternalLocationsTable, sourceReferenceService);
-        givenASourceReferenceFor(agencyLocationsTable, sourceReferenceService);
-        givenASourceReferenceFor(movementReasonsTable, sourceReferenceService);
-        givenASourceReferenceFor(offenderBookingsTable, sourceReferenceService);
-        givenASourceReferenceFor(offenderExternalMovementsTable, sourceReferenceService);
-        // offenders is the only table that has no schema - we expect its data to arrive in violations
-        givenNoSourceReferenceFor(offendersTable, sourceReferenceService);
-
-        List<Row> initialDataEveryTable = Arrays.asList(
-                createRow(1, "2023-11-13 10:00:00.000000", Insert, "1"),
-                createRow(2, "2023-11-13 10:00:00.000000", Insert, "2")
-        );
-
-        givenRawDataIsAddedToEveryTable(initialDataEveryTable);
-
-        whenTheJobRuns();
-
-        thenStructuredCuratedAndOperationalDataStoreContainForPK(agencyInternalLocationsTable, "1", 1);
-        thenStructuredCuratedAndOperationalDataStoreContainForPK(agencyLocationsTable, "1", 1);
-        thenStructuredCuratedAndOperationalDataStoreContainForPK(movementReasonsTable, "1", 1);
-        thenStructuredCuratedAndOperationalDataStoreContainForPK(offenderBookingsTable, "1", 1);
-        thenStructuredCuratedAndOperationalDataStoreContainForPK(offenderExternalMovementsTable, "1", 1);
-
-        thenStructuredCuratedAndOperationalDataStoreContainForPK(agencyInternalLocationsTable, "2", 2);
-        thenStructuredCuratedAndOperationalDataStoreContainForPK(agencyLocationsTable, "2", 2);
-        thenStructuredCuratedAndOperationalDataStoreContainForPK(movementReasonsTable, "2", 2);
-        thenStructuredCuratedAndOperationalDataStoreContainForPK(offenderBookingsTable, "2", 2);
-        thenStructuredCuratedAndOperationalDataStoreContainForPK(offenderExternalMovementsTable, "2", 2);
-
-        thenStructuredCuratedAndOperationalDataStoreDoNotContainPK(offendersTable, 1);
-        thenStructuredCuratedAndOperationalDataStoreDoNotContainPK(offendersTable, 2);
-        thenStructuredViolationsContainsForPK(offendersTable, "1", 1);
-        thenStructuredViolationsContainsForPK(offendersTable, "2", 2);
-    }
-
-    private void whenTheJobRuns() {
-        underTest.runJob(spark);
-    }
-
-    private void givenDependenciesAreInjected() {
-        // Manually creating dependencies because Micronaut test injection is not working
-        JobProperties properties = new JobProperties();
-        SparkSessionProvider sparkSessionProvider = new SparkSessionProvider();
-        TableDiscoveryService tableDiscoveryService = new TableDiscoveryService(arguments, configService);
-        DataStorageService storageService = new DataStorageService(arguments);
-        S3DataProvider dataProvider = new S3DataProvider(arguments);
-        ViolationService violationService = new ViolationService(arguments, storageService, dataProvider, tableDiscoveryService);
-        ValidationService validationService = new ValidationService(violationService);
-        StructuredZoneLoad structuredZoneLoad = new StructuredZoneLoad(arguments, storageService, violationService);
-        CuratedZoneLoad curatedZoneLoad = new CuratedZoneLoad(arguments, storageService, violationService);
-        OperationalDataStoreTransformation operationalDataStoreTransformation = new OperationalDataStoreTransformation();
-        ConnectionPoolProvider connectionPoolProvider = new ConnectionPoolProvider();
-        OperationalDataStoreDataAccess operationalDataStoreDataAccess =
-                new OperationalDataStoreDataAccess(connectionDetailsService, connectionPoolProvider);
-        OperationalDataStoreService operationalDataStoreService =
-                new OperationalDataStoreServiceImpl(arguments, operationalDataStoreTransformation, operationalDataStoreDataAccess);
-        BatchProcessor batchProcessor = new BatchProcessor(structuredZoneLoad, curatedZoneLoad, validationService, operationalDataStoreService);
-        underTest = new DataHubBatchJob(
-                arguments,
-                properties,
-                sparkSessionProvider,
-                tableDiscoveryService,
-                batchProcessor,
-                dataProvider,
-                sourceReferenceService,
-                violationService
-        );
-    }
-
-    private void givenGlobPatternIsConfigured() {
-        // Pattern for data written by Spark as input in tests instead of by DMS
-        when(arguments.getBatchLoadFileGlobPattern()).thenReturn("part-*parquet");
-    }
+//    @Mock
+//    private JobArguments arguments;
+//    @Mock
+//    private SourceReferenceService sourceReferenceService;
+//    @Mock
+//    private ConfigService configService;
+//    @Mock
+//    private OperationalDataStoreConnectionDetailsService connectionDetailsService;
+//    private DataHubBatchJob underTest;
+//
+//
+//    @BeforeEach
+//    public void setUp() throws SQLException {
+//        givenDatastoreCredentials(connectionDetailsService);
+//        givenPathsAreConfigured(arguments);
+//        givenTableConfigIsConfigured(arguments, configService);
+//        givenGlobPatternIsConfigured();
+//        givenRetrySettingsAreConfigured(arguments);
+//        givenDependenciesAreInjected();
+//        givenSchemaExists(inputSchemaName);
+//    }
+//
+//    @Test
+//    public void shouldRunTheJobEndToEndApplyingSomeCDCMessagesAndWritingViolations() throws SQLException {
+//        givenASourceReferenceFor(agencyInternalLocationsTable, sourceReferenceService);
+//        givenASourceReferenceFor(agencyLocationsTable, sourceReferenceService);
+//        givenASourceReferenceFor(movementReasonsTable, sourceReferenceService);
+//        givenASourceReferenceFor(offenderBookingsTable, sourceReferenceService);
+//        givenASourceReferenceFor(offenderExternalMovementsTable, sourceReferenceService);
+//        // offenders is the only table that has no schema - we expect its data to arrive in violations
+//        givenNoSourceReferenceFor(offendersTable, sourceReferenceService);
+//
+//        List<Row> initialDataEveryTable = Arrays.asList(
+//                createRow(1, "2023-11-13 10:00:00.000000", Insert, "1"),
+//                createRow(2, "2023-11-13 10:00:00.000000", Insert, "2")
+//        );
+//
+//        givenRawDataIsAddedToEveryTable(initialDataEveryTable);
+//
+//        whenTheJobRuns();
+//
+//        thenStructuredCuratedAndOperationalDataStoreContainForPK(agencyInternalLocationsTable, "1", 1);
+//        thenStructuredCuratedAndOperationalDataStoreContainForPK(agencyLocationsTable, "1", 1);
+//        thenStructuredCuratedAndOperationalDataStoreContainForPK(movementReasonsTable, "1", 1);
+//        thenStructuredCuratedAndOperationalDataStoreContainForPK(offenderBookingsTable, "1", 1);
+//        thenStructuredCuratedAndOperationalDataStoreContainForPK(offenderExternalMovementsTable, "1", 1);
+//
+//        thenStructuredCuratedAndOperationalDataStoreContainForPK(agencyInternalLocationsTable, "2", 2);
+//        thenStructuredCuratedAndOperationalDataStoreContainForPK(agencyLocationsTable, "2", 2);
+//        thenStructuredCuratedAndOperationalDataStoreContainForPK(movementReasonsTable, "2", 2);
+//        thenStructuredCuratedAndOperationalDataStoreContainForPK(offenderBookingsTable, "2", 2);
+//        thenStructuredCuratedAndOperationalDataStoreContainForPK(offenderExternalMovementsTable, "2", 2);
+//
+//        thenStructuredCuratedAndOperationalDataStoreDoNotContainPK(offendersTable, 1);
+//        thenStructuredCuratedAndOperationalDataStoreDoNotContainPK(offendersTable, 2);
+//        thenStructuredViolationsContainsForPK(offendersTable, "1", 1);
+//        thenStructuredViolationsContainsForPK(offendersTable, "2", 2);
+//    }
+//
+//    private void whenTheJobRuns() {
+//        underTest.runJob(spark);
+//    }
+//
+//    private void givenDependenciesAreInjected() {
+//        // Manually creating dependencies because Micronaut test injection is not working
+//        JobProperties properties = new JobProperties();
+//        SparkSessionProvider sparkSessionProvider = new SparkSessionProvider();
+//        TableDiscoveryService tableDiscoveryService = new TableDiscoveryService(arguments, configService);
+//        DataStorageService storageService = new DataStorageService(arguments);
+//        S3DataProvider dataProvider = new S3DataProvider(arguments);
+//        ViolationService violationService = new ViolationService(arguments, storageService, dataProvider, tableDiscoveryService);
+//        ValidationService validationService = new ValidationService(violationService);
+//        StructuredZoneLoad structuredZoneLoad = new StructuredZoneLoad(arguments, storageService, violationService);
+//        CuratedZoneLoad curatedZoneLoad = new CuratedZoneLoad(arguments, storageService, violationService);
+//        OperationalDataStoreTransformation operationalDataStoreTransformation = new OperationalDataStoreTransformation();
+//        ConnectionPoolProvider connectionPoolProvider = new ConnectionPoolProvider();
+//        OperationalDataStoreDataAccess operationalDataStoreDataAccess =
+//                new OperationalDataStoreDataAccess(connectionDetailsService, connectionPoolProvider);
+//        OperationalDataStoreService operationalDataStoreService =
+//                new OperationalDataStoreServiceImpl(arguments, operationalDataStoreTransformation, operationalDataStoreDataAccess);
+//        BatchProcessor batchProcessor = new BatchProcessor(structuredZoneLoad, curatedZoneLoad, validationService, operationalDataStoreService);
+//        underTest = new DataHubBatchJob(
+//                arguments,
+//                properties,
+//                sparkSessionProvider,
+//                tableDiscoveryService,
+//                batchProcessor,
+//                dataProvider,
+//                sourceReferenceService,
+//                violationService
+//        );
+//    }
+//
+//    private void givenGlobPatternIsConfigured() {
+//        // Pattern for data written by Spark as input in tests instead of by DMS
+//        when(arguments.getBatchLoadFileGlobPattern()).thenReturn("part-*parquet");
+//    }
 }
