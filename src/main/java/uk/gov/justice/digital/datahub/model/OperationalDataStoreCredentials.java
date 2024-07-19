984b2da3a64e17037aa2e2d1ee158a73565103f1
Tom Ogle <tom.ogle@digital.justice.gov.uk>
2024-07-19 15:49:06 +0100

    DPR2-901: Fix lombok annotations required for deserialising credentials.

--- src/main/java/uk/gov/justice/digital/datahub/model/OperationalDataStoreCredentials.java
+++ src/main/java/uk/gov/justice/digital/datahub/model/OperationalDataStoreCredentials.java
@@ -1,9 +1,13 @@
 package uk.gov.justice.digital.datahub.model;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
+@NoArgsConstructor
+@AllArgsConstructor
 @Data
 public class OperationalDataStoreCredentials {
-    private final String username;
-    private final String password;
+    private String username;
+    private String password;
 }
