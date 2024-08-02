067cc8ea5df24a7e3ec2911d247e854cd807f3d7
Tom Ogle <tom.ogle@digital.justice.gov.uk>
2024-08-02 11:45:35 +0100

    DPR2-1008: Remove public modifier

--- src/test/java/uk/gov/justice/digital/datahub/model/SourceReferenceTest.java
+++ src/test/java/uk/gov/justice/digital/datahub/model/SourceReferenceTest.java
@@ -8,7 +8,7 @@ import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-public class SourceReferenceTest {
+class SourceReferenceTest {
 
     private SourceReference underTest;
 
