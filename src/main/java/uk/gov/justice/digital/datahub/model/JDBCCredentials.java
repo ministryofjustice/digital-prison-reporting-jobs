87ba98580fa3167ea55eb44915ce843b4ebbcce3
Tom Ogle <tom.ogle@digital.justice.gov.uk>
2024-09-09 12:04:46 +0100

    DPR2-1117: Ignore json fields that aren't known during deserialisation

--- src/main/java/uk/gov/justice/digital/datahub/model/JDBCCredentials.java
+++ src/main/java/uk/gov/justice/digital/datahub/model/JDBCCredentials.java
@@ -1,9 +1,11 @@
 package uk.gov.justice.digital.datahub.model;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+@JsonIgnoreProperties(ignoreUnknown = true)
 @NoArgsConstructor
 @AllArgsConstructor
 @Data
