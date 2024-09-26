83134850a42c33259eee5054f83e23458f303005
Tom Ogle <tom.ogle@digital.justice.gov.uk>
2024-09-26 17:02:20 +0100

    DPR2-1127: Fail early with null connection or credential details

--- src/main/java/uk/gov/justice/digital/datahub/model/JDBCCredentials.java
+++ src/main/java/uk/gov/justice/digital/datahub/model/JDBCCredentials.java
@@ -1,6 +1,7 @@
 package uk.gov.justice.digital.datahub.model;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -10,6 +11,8 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @Data
 public class JDBCCredentials {
+    @JsonProperty(required = true)
     private String username;
+    @JsonProperty(required = true)
     private String password;
 }
