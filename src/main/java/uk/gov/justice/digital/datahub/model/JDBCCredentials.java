4c20fdbf54a347ddc759cbaf34ca58d2557a8c50
Tom Ogle <tom.ogle@digital.justice.gov.uk>
2024-09-27 10:36:23 +0100

    DPR2-1127: Remove JsonProperty annotation which does not prevent deserialisation of null username.

--- src/main/java/uk/gov/justice/digital/datahub/model/JDBCCredentials.java
+++ src/main/java/uk/gov/justice/digital/datahub/model/JDBCCredentials.java
@@ -1,7 +1,6 @@
 package uk.gov.justice.digital.datahub.model;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
-import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -11,8 +10,6 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @Data
 public class JDBCCredentials {
-    @JsonProperty(value = "username", required = true)
     private String username;
-    @JsonProperty(value = "password", required = true)
     private String password;
 }
