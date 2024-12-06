e47c303deb4593dca0720e69de641294f4988727
Tom Ogle <tom.ogle@digital.justice.gov.uk>
2024-12-06 16:15:20 +0000

    DPR2-1133 : Linting change

--- src/test/java/uk/gov/justice/digital/datahub/model/PrimaryKeyTest.java
+++ src/test/java/uk/gov/justice/digital/datahub/model/PrimaryKeyTest.java
@@ -14,7 +14,7 @@ import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.contains;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-public class PrimaryKeyTest {
+class PrimaryKeyTest {
 
     @Test
     void shouldGetSparkConditionForSingleColumnPK() {
