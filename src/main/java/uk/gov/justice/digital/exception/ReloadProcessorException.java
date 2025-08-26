1c6fded0a9065a4bf880c4dce77623a5e49e4d81
Kolade Adewuyi <kolade.adewuyi@digital.justice.gov.uk>
2025-08-26 22:13:45 +0100

    DHS-411: Handle addition of nullable column(s)

---
+++ src/main/java/uk/gov/justice/digital/exception/ReloadProcessorException.java
@@ -0,0 +1,9 @@
+package uk.gov.justice.digital.exception;
+
+public class ReloadProcessorException extends RuntimeException {
+    private static final long serialVersionUID = -2244810603090098293L;
+
+    public ReloadProcessorException(String message) {
+        super(message);
+    }
+}
