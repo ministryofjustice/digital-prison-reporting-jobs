349049e84d97766223cf28c1ade3f8c7fa7ddc95
koladeadewuyi-moj <136330532+koladeadewuyi-moj@users.noreply.github.com>
2025-09-15 14:23:52 +0100

    DHS-447: Increase the default dpr.orchestration.max.attempts to 40 (#377)

--- src/main/java/uk/gov/justice/digital/config/JobArguments.java
+++ src/main/java/uk/gov/justice/digital/config/JobArguments.java
@@ -133,7 +133,7 @@ public class JobArguments {
     static final String ORCHESTRATION_WAIT_INTERVAL_SECONDS = "dpr.orchestration.wait.interval.seconds";
     static final int DEFAULT_ORCHESTRATION_WAIT_INTERVAL_SECONDS = 40;
     static final String ORCHESTRATION_MAX_ATTEMPTS = "dpr.orchestration.max.attempts";
-    static final int DEFAULT_ORCHESTRATION_MAX_ATTEMPTS = 20;
+    static final int DEFAULT_ORCHESTRATION_MAX_ATTEMPTS = 40;
     static final String STOP_GLUE_INSTANCE_JOB_NAME = "dpr.stop.glue.instance.job.name";
     static final String DMS_REPLICATION_TASK_ID = "dpr.dms.replication.task.id";
     static final String CDC_DMS_REPLICATION_TASK_ID = "dpr.cdc.dms.replication.task.id";
