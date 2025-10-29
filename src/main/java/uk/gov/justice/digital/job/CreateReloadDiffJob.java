5c5d3c5f9a2538591173d89dd8f64026d9b89772
Tom Ogle <tom.ogle@justice.gov.uk>
2025-10-29 13:03:44 +0000

    DHS-469: Fix flaky test by injecting a clock that can be stubbed/mocked for tests.

--- src/main/java/uk/gov/justice/digital/job/CreateReloadDiffJob.java
+++ src/main/java/uk/gov/justice/digital/job/CreateReloadDiffJob.java
@@ -5,7 +5,6 @@ import lombok.val;
 import org.apache.commons.lang3.tuple.ImmutablePair;
 import org.apache.spark.sql.Encoders;
 import org.apache.spark.sql.SparkSession;
-import org.apache.spark.sql.catalyst.encoders.RowEncoder;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import picocli.CommandLine;
