51f24860eca70a64f503addb553431c882692d41
Tom Ogle <tom.ogle@justice.gov.uk>
2026-01-08 10:56:41 +0000

    DHS-580: Spike fix for s3 file transfer failures at glue v4.

--- src/main/java/uk/gov/justice/digital/client/s3/S3ClientProvider.java
+++ src/main/java/uk/gov/justice/digital/client/s3/S3ClientProvider.java
@@ -1,15 +1,35 @@
 package uk.gov.justice.digital.client.s3;
 
+import com.amazonaws.ClientConfiguration;
 import com.amazonaws.services.s3.AmazonS3;
 import com.amazonaws.services.s3.AmazonS3ClientBuilder;
 import uk.gov.justice.digital.client.ClientProvider;
+import uk.gov.justice.digital.config.JobArguments;
 
+import javax.inject.Inject;
 import javax.inject.Singleton;
 
 @Singleton
 public class S3ClientProvider implements ClientProvider<AmazonS3> {
+
+    private final JobArguments jobArguments;
+
+    @Inject
+    public S3ClientProvider(JobArguments jobArguments) {
+        this.jobArguments = jobArguments;
+    }
+
     @Override
     public AmazonS3 getClient() {
-        return AmazonS3ClientBuilder.defaultClient();
+        // The maximum number of connections to S3
+        // Give us some wiggle room just in case there are other tasks
+        int extraBufferForOtherTasks = 10;
+        int maxConnections = jobArguments.getFileTransferParallelism() + extraBufferForOtherTasks;
+
+        ClientConfiguration clientConfiguration = new ClientConfiguration()
+                .withMaxConnections(maxConnections);
+        return AmazonS3ClientBuilder.standard()
+                .withClientConfiguration(clientConfiguration)
+                .build();
     }
 }
