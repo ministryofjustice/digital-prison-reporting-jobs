129e3edbbd8020a0189a179b45c9a182540d5c87
Tom Ogle <tom.ogle@justice.gov.uk>
2026-01-15 14:09:31 +0000

    DHS-550: Better naming for flag that controls whether to report metrics.

--- src/main/java/uk/gov/justice/digital/service/metrics/DisabledMetricReportingService.java
+++ src/main/java/uk/gov/justice/digital/service/metrics/DisabledMetricReportingService.java
@@ -4,10 +4,10 @@ import io.micronaut.context.annotation.Requires;
 import jakarta.inject.Singleton;
 import uk.gov.justice.digital.service.datareconciliation.model.DataReconciliationResults;
 
-import static uk.gov.justice.digital.config.JobArguments.REPORT_RESULTS_TO_CLOUDWATCH;
+import static uk.gov.justice.digital.config.JobArguments.REPORT_METRICS_TO_CLOUDWATCH;
 
 @Singleton
-@Requires(missingProperty = REPORT_RESULTS_TO_CLOUDWATCH)
+@Requires(missingProperty = REPORT_METRICS_TO_CLOUDWATCH)
 public class DisabledMetricReportingService implements MetricReportingService {
     @Override
     public void reportDataReconciliationResults(DataReconciliationResults dataReconciliationResults) {
